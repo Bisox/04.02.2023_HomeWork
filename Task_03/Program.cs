28ee2660003f93b28825efb65d3a3098237b480c
Evgeniy Bratenkov <giertrut@yandex.ru>
2023-02-08 13:43:36 +0300

    03

---
+++ Task_03/Program.cs
@@ -0,0 +1,58 @@
+﻿//  Задача 3: Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] CreateMatrix()
+{
+    int[,] matrix = new int[4, 4];
+
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(1, 10);
+        }
+    }
+    return matrix;
+}
+
+
+void AverageElements(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(1); i++)
+    {
+        double sum = 0;
+        for (int j = 0; j < array.GetLength(0); j++)
+        {
+            sum += array[j, i];
+        }
+        Console.WriteLine($"Среднее арифметическое столбца {i + 1}: {sum / array.GetLength(0)};");
+
+    }
+
+}
+
+
+int[,] matrix = CreateMatrix();
+PrintArray(matrix);
+Console.WriteLine();
+AverageElements(matrix);
